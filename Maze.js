101f57f803da70719c02167c5f471cabd165e45c
SmithC <02Cade.Smith@gulllakecs.org>
2024-01-22 08:27:14 -0500

    Start! text & Vocal

--- Maze.js
+++ Maze.js
@@ -1,5 +1,17 @@
 // Team Members: Cade Smith & Carson Grant  
 //Name: Cade Smith 
 // Date: 1.22.2024
-Program: Maze
-//Bolt ID: 
+//Program: Maze
+//Bolt ID: B2F8
+
+async function startProgram() {
+
+	setMainLed({ r: 255, g: 255, b: 255 });
+
+	await speak("Start", true); 
+
+	await scrollMatrixText('Start!', { r: 66, g: 56, b: 255 }, 15, true)
+
+
+
+}
